55e6d3840485997b06f3651fed02d721ec726213
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-08-23 11:08:58 -0300

    feat: criando document

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pizzoares - Faça seu login</title>
+      </Head>
+      <div>
+        <h1>Soares Pizzaria</h1>
+      </div>
+    </>
+  )
+}
