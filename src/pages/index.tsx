3de42f9df3699a9cb6a89dbaadb967379b3f54c6
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-08-23 11:39:23 -0300

    feat: criando tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import Head from 'next/head'
+import styles from '../../styles/home.module.scss'
+import Image from 'next/image'
+import logoImg from '../../public/logo.svg'
+import { Input } from '../components/ui/input'
 
 export default function Home() {
   return (
     <>
       <Head>
+        logoImg
         <title>Pizzoares - Faça seu login</title>
       </Head>
-      <div>
-        <h1>Soares Pizzaria</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="logo sujeitopizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="seu email" type="text" />
+            <Input placeholder="sua senha" type="password" />
+          </form>
+        </div>
       </div>
     </>
   )
