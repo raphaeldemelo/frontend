1b0e6387687b4aae120a61bc5b4b55cc5b6a44d6
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-09-26 17:10:47 -0300

    feat: página categoria

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,42 @@
+import { useState, FormEvent } from 'react'
+import Head from 'next/head'
+import { Header } from '../../components/Header'
+import styles from './styles.module.scss'
+
+export default function Category() {
+  const [name, setName] = useState('')
+
+  async function handleRegister(event: Event) {
+    event.preventDefault()
+    alert('Categoria ' + name)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Nova Categoria - Sujeito Pizzaria</title>
+      </Head>
+      <div>
+        <Header />
+
+        <main className={styles.container}>
+          <h1>Cadastrar categorias</h1>
+
+          <form className={styles.form} onSubmit={handleRegister}>
+            <input
+              type="text"
+              placeholder="Digite o nome da categoria"
+              className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <button type="submit" className={styles.buttonAdd}>
+              Cadastrar
+            </button>
+          </form>
+        </main>
+      </div>
+    </>
+  )
+}
